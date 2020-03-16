4c3bc89f52dfea009bc5359f4045b4f78bd7ce4f
Jan Mikeš <j.mikes@me.com>
2020-03-16 18:55:37 +0100

    Added failing test

--- src/LinkBasicAuthDecorator.php
+++ src/LinkBasicAuthDecorator.php
@@ -4,5 +4,26 @@ namespace JanMikes\Slacker;
 
 final class LinkBasicAuthDecorator
 {
+	/**
+	 * @var string
+	 */
+	private $username;
 
+	/**
+	 * @var string
+	 */
+	private $password;
+
+
+	public function __construct(string $username, string $password)
+	{
+		$this->username = $username;
+		$this->password = $password;
+	}
+
+
+	public function decorate(string $url): string
+	{
+		return $url;
+	}
 }
--- tests/LinkBasicAuthDecoratorTest.php
+++ tests/LinkBasicAuthDecoratorTest.php
@@ -2,9 +2,16 @@
 
 namespace JanMikes\Slacker\Tests;
 
+use JanMikes\Slacker\LinkBasicAuthDecorator;
 use PHPUnit\Framework\TestCase;
 
 class LinkBasicAuthDecoratorTest extends TestCase
 {
+	public function testDecorate(): void
+	{
+		$linkDecorator = new LinkBasicAuthDecorator('user', 'pass');
+		$decoratedLink = $linkDecorator->decorate('https://google.com');
 
+		$this->assertSame('https://user:pass@google.com', $decoratedLink);
+	}
 }
