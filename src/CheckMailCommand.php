82d5c6366b934fd3851b85771b33e64684f05e23
Jan Mikeš <j.mikes@me.com>
2020-03-17 13:01:23 +0100

    Decoupling to ExchangeMailClient

--- src/CheckMailCommand.php
+++ src/CheckMailCommand.php
@@ -2,24 +2,7 @@
 
 namespace JanMikes\Slacker;
 
-use jamesiarmes\PhpEws\ArrayType\NonEmptyArrayOfBaseFolderIdsType;
-use jamesiarmes\PhpEws\Autodiscover;
-use jamesiarmes\PhpEws\Enumeration\DefaultShapeNamesType;
-use jamesiarmes\PhpEws\Enumeration\DistinguishedFolderIdNameType;
-use jamesiarmes\PhpEws\Enumeration\ResponseClassType;
-use jamesiarmes\PhpEws\Enumeration\UnindexedFieldURIType;
-use jamesiarmes\PhpEws\Request\FindItemType;
-use jamesiarmes\PhpEws\Type\AndType;
-use jamesiarmes\PhpEws\Type\ConstantValueType;
-use jamesiarmes\PhpEws\Type\ContainsExpressionType;
-use jamesiarmes\PhpEws\Type\DistinguishedFolderIdType;
-use jamesiarmes\PhpEws\Type\FieldURIOrConstantType;
-use jamesiarmes\PhpEws\Type\IsEqualToType;
-use jamesiarmes\PhpEws\Type\IsGreaterThanOrEqualToType;
-use jamesiarmes\PhpEws\Type\ItemResponseShapeType;
-use jamesiarmes\PhpEws\Type\PathToUnindexedFieldType;
-use jamesiarmes\PhpEws\Type\QueryStringType;
-use jamesiarmes\PhpEws\Type\RestrictionType;
+use JanMikes\Slacker\ExchangeWebService\MailClient;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -27,46 +10,16 @@ use Symfony\Component\Console\Output\OutputInterface;
 final class CheckMailCommand extends Command
 {
 	/**
-	 * @var string
+	 * @var MailClient
 	 */
-	private $email;
-
-	/**
-	 * @var string
-	 */
-	private $user;
-
-	/**
-	 * @var string
-	 */
-	private $password;
-
-	/**
-	 * @var string
-	 */
-	private $subject;
-
-	/**
-	 * @var string
-	 */
-	private $sender;
+	private $mailClient;
 
 
-	public function __construct(
-		string $email,
-		string $user,
-		string $password,
-		string $subject,
-		string $sender
-	)
+	public function __construct(MailClient $mailClient)
 	{
-		$this->email = $email;
-		$this->user = $user;
-		$this->password = $password;
-		$this->subject = $subject;
-		$this->sender = $sender;
-
 		parent::__construct();
+
+		$this->mailClient = $mailClient;
 	}
 
 
@@ -78,79 +31,10 @@ final class CheckMailCommand extends Command
 
 	protected function execute(InputInterface $input, OutputInterface $output): int
 	{
-		$client = Autodiscover::getEWS($this->email, $this->password, $this->user);
+		$messages = $this->mailClient->getMessagesIds();
 
-		$request = new FindItemType();
-		$request->ParentFolderIds = new NonEmptyArrayOfBaseFolderIdsType();
-
-		$start_date = new \DateTimeImmutable('yesterday');
-
-		$subject = new ContainsExpressionType();
-		$subject->FieldURI = new PathToUnindexedFieldType();
-		$subject->FieldURI->FieldURI = UnindexedFieldURIType::ITEM_SUBJECT;
-		$subject->Constant = new ConstantValueType();
-		$subject->Constant->Value = $this->subject;
-
-		$sender = new IsEqualToType();
-		$sender->FieldURI = new PathToUnindexedFieldType();
-		$sender->FieldURI->FieldURI = UnindexedFieldURIType::MESSAGE_SENDER;
-		$sender->FieldURIOrConstant = new FieldURIOrConstantType();
-		$sender->FieldURIOrConstant->Constant = new ConstantValueType();
-		$sender->FieldURIOrConstant->Constant->Value = $this->sender;
-
-		// Build the start date restriction.
-		$greater_than = new IsGreaterThanOrEqualToType();
-		$greater_than->FieldURI = new PathToUnindexedFieldType();
-		$greater_than->FieldURI->FieldURI = UnindexedFieldURIType::ITEM_DATE_TIME_RECEIVED;
-		$greater_than->FieldURIOrConstant = new FieldURIOrConstantType();
-		$greater_than->FieldURIOrConstant->Constant = new ConstantValueType();
-		$greater_than->FieldURIOrConstant->Constant->Value = $start_date->format('c');
-
-
-		// Build the restriction.
-		$request->Restriction = new RestrictionType();
-		$request->Restriction->And = new AndType();
-		$request->Restriction->And->IsGreaterThanOrEqualTo = $greater_than;
-		$request->Restriction->And->Contains = $subject;
-		$request->Restriction->And->IsEqualTo = $sender;
-
-
-		// Return all message properties.
-		$request->ItemShape = new ItemResponseShapeType();
-		$request->ItemShape->BaseShape = DefaultShapeNamesType::ALL_PROPERTIES;
-
-		$request->QueryString = new QueryStringType();
-
-		// Search in the user's inbox.
-		$folder_id = new DistinguishedFolderIdType();
-		$folder_id->Id = DistinguishedFolderIdNameType::INBOX;
-		$request->ParentFolderIds->DistinguishedFolderId[] = $folder_id;
-
-		$response = $client->FindItem($request);
-
-		// Iterate over the results, printing any error messages or message subjects.
-		$response_messages = $response->ResponseMessages->FindItemResponseMessage;
-		foreach ($response_messages as $response_message) {
-			// Make sure the request succeeded.
-			if ($response_message->ResponseClass !== ResponseClassType::SUCCESS) {
-				$code = $response_message->ResponseCode;
-				$message = $response_message->MessageText;
-				fwrite(
-					STDERR,
-					"Failed to search for messages with \"$code: $message\"\n"
-				);
-				continue;
-			}
-
-			// Iterate over the messages that were found, printing the subject for each.
-			$items = $response_message->RootFolder->Items->Message;
-			foreach ($items as $item) {
-				$subject = $item->Subject;
-				$id = $item->ItemId->Id;
-				fwrite(STDOUT, "$subject: $id\n");
-
-				// @TODO: get the item, because we need its content
-			}
+		foreach ($messages as $messageId) {
+			$output->writeln($messageId);
 		}
 
 		return 0;
---
+++ src/ExchangeWebService/MailClient.php
@@ -0,0 +1,135 @@
+<?php declare (strict_types=1);
+
+namespace JanMikes\Slacker\ExchangeWebService;
+
+use jamesiarmes\PhpEws\ArrayType\NonEmptyArrayOfBaseFolderIdsType;
+use jamesiarmes\PhpEws\Autodiscover;
+use jamesiarmes\PhpEws\Client;
+use jamesiarmes\PhpEws\Enumeration\DefaultShapeNamesType;
+use jamesiarmes\PhpEws\Enumeration\DistinguishedFolderIdNameType;
+use jamesiarmes\PhpEws\Enumeration\ResponseClassType;
+use jamesiarmes\PhpEws\Enumeration\UnindexedFieldURIType;
+use jamesiarmes\PhpEws\Request\FindItemType;
+use jamesiarmes\PhpEws\Type\AndType;
+use jamesiarmes\PhpEws\Type\ConstantValueType;
+use jamesiarmes\PhpEws\Type\ContainsExpressionType;
+use jamesiarmes\PhpEws\Type\DistinguishedFolderIdType;
+use jamesiarmes\PhpEws\Type\FieldURIOrConstantType;
+use jamesiarmes\PhpEws\Type\IsEqualToType;
+use jamesiarmes\PhpEws\Type\IsGreaterThanOrEqualToType;
+use jamesiarmes\PhpEws\Type\ItemResponseShapeType;
+use jamesiarmes\PhpEws\Type\PathToUnindexedFieldType;
+use jamesiarmes\PhpEws\Type\RestrictionType;
+
+final class MailClient
+{
+	/**
+	 * @var Client
+	 */
+	private $client;
+
+	/**
+	 * @var string
+	 */
+	private $messageSubject;
+
+	/**
+	 * @var string
+	 */
+	private $messageSender;
+
+
+	public function __construct(
+		string $exchangeEmail,
+		string $exchangeUser,
+		string $exchangePassword,
+		string $messageSubject,
+		string $messageSender
+	)
+	{
+		$client = Autodiscover::getEWS($exchangeEmail, $exchangePassword, $exchangeUser);
+
+		if (!$client) {
+			throw new \RuntimeException('Could not autodiscover exchange settings from credentials.');
+		}
+
+		$this->client = $client;
+		$this->messageSubject = $messageSubject;
+		$this->messageSender = $messageSender;
+	}
+
+
+	/**
+	 * @return string[]
+	 */
+	public function getMessagesIds(): array
+	{
+		$request = new FindItemType();
+		$request->ParentFolderIds = new NonEmptyArrayOfBaseFolderIdsType();
+
+		$subject = new ContainsExpressionType();
+		$subject->FieldURI = new PathToUnindexedFieldType();
+		$subject->FieldURI->FieldURI = UnindexedFieldURIType::ITEM_SUBJECT;
+		$subject->Constant = new ConstantValueType();
+		$subject->Constant->Value = $this->messageSubject;
+
+		$sender = new IsEqualToType();
+		$sender->FieldURI = new PathToUnindexedFieldType();
+		$sender->FieldURI->FieldURI = UnindexedFieldURIType::MESSAGE_SENDER;
+		$sender->FieldURIOrConstant = new FieldURIOrConstantType();
+		$sender->FieldURIOrConstant->Constant = new ConstantValueType();
+		$sender->FieldURIOrConstant->Constant->Value = $this->messageSender;
+
+		// Build the start date restriction.
+		$startDate = new IsGreaterThanOrEqualToType();
+		$startDate->FieldURI = new PathToUnindexedFieldType();
+		$startDate->FieldURI->FieldURI = UnindexedFieldURIType::ITEM_DATE_TIME_RECEIVED;
+		$startDate->FieldURIOrConstant = new FieldURIOrConstantType();
+		$startDate->FieldURIOrConstant->Constant = new ConstantValueType();
+		$startDate->FieldURIOrConstant->Constant->Value = (new \DateTimeImmutable('yesterday'))->format('c');
+
+		// Build the restriction.
+		$request->Restriction = new RestrictionType();
+		$request->Restriction->And = new AndType();
+		$request->Restriction->And->IsGreaterThanOrEqualTo = $startDate;
+		$request->Restriction->And->Contains = $subject;
+		$request->Restriction->And->IsEqualTo = $sender;
+
+		// Return just ids.
+		$request->ItemShape = new ItemResponseShapeType();
+		$request->ItemShape->BaseShape = DefaultShapeNamesType::ID_ONLY;
+
+		// Search in the user's inbox.
+		$folder = new DistinguishedFolderIdType();
+		$folder->Id = DistinguishedFolderIdNameType::INBOX;
+		$request->ParentFolderIds->DistinguishedFolderId[] = $folder;
+
+		$response = $this->client->FindItem($request);
+		$ids = [];
+
+		// Iterate over the results, printing any error messages or message subjects.
+		foreach ($response->ResponseMessages->FindItemResponseMessage as $foundItem) {
+			// Make sure the request succeeded.
+			if ($foundItem->ResponseClass !== ResponseClassType::SUCCESS) {
+				$code = $foundItem->ResponseCode;
+				$message = $foundItem->MessageText;
+
+				// @TODO: Throw exception instead
+				fwrite(
+					STDERR,
+					"Failed to search for messages with \"$code: $message\"\n"
+				);
+				continue;
+			}
+
+			// Iterate over the messages that were found, printing the subject for each.
+			$items = $foundItem->RootFolder->Items->Message;
+			foreach ($items as $item) {
+				// $id = $item->ItemId->ChangeKey; // What is it?
+				$ids[] = $item->ItemId->Id;
+			}
+		}
+
+		return $ids;
+	}
+}
